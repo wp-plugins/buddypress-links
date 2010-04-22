643c3e2063fb83933f000ca7102ec1d166b06aca
MrMaz <MrMaz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-22 21:33:08 +0000

    making some strings translatable
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-links/trunk@232210 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/bp-links-admin/index.php
+++ themes/bp-links-admin/index.php
@@ -8,15 +8,15 @@
 	<div id="icon-link-manager" class="icon32"><br /></div>
 	<h2><?php _e( 'BuddyPress Links', 'buddypress-links' ) ?></h2>
 
-	<h3>Thank you for installing BuddyPress Links!</h3>
+	<h3><?php _e( 'Thank you for installing BuddyPress Links!', 'buddypress-links' ) ?></h3>
 
-	<p>Administrative options:</p>
+	<p><?php _e( 'Administrative options:', 'buddypress-links' ) ?></p>
 	<ul>
-		<li><a href="?page=buddypress-links-admin-links">Manage Links</a></li>
-		<li><a href="?page=buddypress-links-admin-cats">Edit Categories</a></li>
+		<li><a href="?page=buddypress-links-admin-links"><?php _e( 'Manage Links', 'buddypress-links' ) ?></a></li>
+		<li><a href="?page=buddypress-links-admin-cats"><?php _e( 'Edit Categories', 'buddypress-links' ) ?></a></li>
 	</ul>
 
-	<p>Where to get support:</p>
+	<p><?php _e( 'Where to get support:', 'buddypress-links' ) ?></p>
 	<ul>
 		<li><a href="http://buddypress.org/forums/topic/buddypress-links-04x-releases-and-support" target="_blank">BuddyPress.org Forums</a></li>
 		<li><a href="http://buddypress.org/groups/buddypress-links" target="_blank">Official BuddyPress Links Group</a></li>
@@ -24,7 +24,7 @@
 		<li><a href="http://plugins.trac.wordpress.org/browser/buddypress-links/" target="_blank">Trac Browser</a></li>
 	</ul>
 
-	<p>About the author:</p>
+	<p><?php _e( 'About the author:', 'buddypress-links' ) ?></p>
 	<ul>
 		<li><a href="http://marshallsorenson.com/" target="_blank">Marshall Sorenson's Blog</a></li>
 		<li><a href="http://buddypress.org/developers/mrmaz/" target="_blank">MrMaz on BuddyPress.org</a></li>
