f8f702c413cb2713a4c6ef7782b5df3031dcfb70
MrMaz <MrMaz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-21 17:50:02 +0000

    added filter to bp_links_is_url_valid
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-links/trunk@302726 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bp-links-core.php
+++ bp-links-core.php
@@ -330,7 +330,7 @@ function bp_links_setup_nav() {
 
 	/* Add 'Links' to the main navigation */
 	$nav_item_name = sprintf( apply_filters( 'bp_links_nav_item_name', __( 'Links <span>(%d)</span>', 'buddypress-links' ) ), bp_links_total_links_for_user() );
-	bp_core_new_nav_item( array( 'name' => $nav_item_name, 'slug' => $bp->links->slug, 'position' => 75, 'screen_function' => 'bp_links_screen_personal_links', 'default_subnav_slug' => 'my-links', 'item_css_id' => $bp->links->id ) );
+	bp_core_new_nav_item( array( 'name' => $nav_item_name, 'slug' => $bp->links->slug, 'position' => 1, 'screen_function' => 'bp_links_screen_personal_links', 'default_subnav_slug' => 'my-links', 'item_css_id' => $bp->links->id ) );
 
 	$links_link = $bp->loggedin_user->domain . $bp->links->slug . '/';
 	
@@ -1337,7 +1337,7 @@ function bp_links_current_link_embed_service() {
 /*** General Link Functions ***************************************************/
 
 function bp_links_is_url_valid( $url ) {
-	return ( preg_match('/^https?:\/\/(([a-z0-9-]+\.)+[a-z]{2,6}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(:[0-9]+)?(\/?|\/\S+)$/iu', $url ) === 1 ) ? true : false;
+	return ( apply_filters( 'bp_links_is_url_valid', ( preg_match('/^https?:\/\/(([a-z0-9-]+\.)+[a-z]{2,6}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(:[0-9]+)?(\/?|\/\S+)$/iu', $url ) === 1 ) ? true : false, $url ) );
 }
 
 function bp_links_check_link_exists( $link_id ) {
