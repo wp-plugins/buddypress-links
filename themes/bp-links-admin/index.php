00a33d8a24fc7a3db33ba97add6ddaa5c3e6450c
MrMaz <MrMaz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-22 10:39:05 +0000

    fixed links
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-links/trunk@302907 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/bp-links-admin/index.php
+++ themes/bp-links-admin/index.php
@@ -18,8 +18,7 @@
 
 	<p><?php _e( 'Where to get support:', 'buddypress-links' ) ?></p>
 	<ul>
-		<li><a href="http://buddypress.org/forums/topic/buddypress-links-04x-releases-and-support" target="_blank">BuddyPress.org Forums</a></li>
-		<li><a href="http://buddypress.org/groups/buddypress-links" target="_blank">Official BuddyPress Links Group</a></li>
+		<li><a href="http://buddypress.org/community/groups/buddypress-links/" target="_blank">Official BuddyPress Links Group</a></li>
 		<li><a href="http://plugins.trac.wordpress.org/log/buddypress-links" target="_blank">Trac Revision Log</a></li>
 		<li><a href="http://plugins.trac.wordpress.org/browser/buddypress-links/" target="_blank">Trac Browser</a></li>
 	</ul>
@@ -27,7 +26,7 @@
 	<p><?php _e( 'About the author:', 'buddypress-links' ) ?></p>
 	<ul>
 		<li><a href="http://marshallsorenson.com/" target="_blank">Marshall Sorenson's Blog</a></li>
-		<li><a href="http://buddypress.org/developers/mrmaz/" target="_blank">MrMaz on BuddyPress.org</a></li>
+		<li><a href="http://buddypress.org/community/members/MrMaz/" target="_blank">MrMaz on BuddyPress.org</a></li>
 	</ul>
 
 </div>
